a9ccfd4c512bb3d96a1ac19a035bc0a14478fb2c
Marcin Makarewicz <marcin@makarewicz.eu>
2022-11-15 23:13:50 +0100

    fix(chrome,worker): process url

--- packages/chrome/src/index.ts
+++ packages/chrome/src/index.ts
@@ -49,71 +49,108 @@ export async function chrome(url: string = "https://zimekk.github.io/robot/") {
   await page.setUserAgent((await browser.userAgent()).replace("Headless", ""));
   await page.setRequestInterception(true);
 
-  page
-    .on("request", (req: HTTPRequest) => {
-      // console.log({
-      //   req: req.url(),
-      //   headers: req.headers(),
-      //   resourceType: req.resourceType(),
-      // });
-      if (["font", "image", "stylesheet"].includes(req.resourceType())) {
-        req.abort();
-      } else {
-        req.continue();
-      }
-    })
-    .on("response", async (res: HTTPResponse) => {
-      const url = res.url();
-      const req = res.request();
-      const headers = res.headers();
+  const result = new Promise(async (resolve) => {
+    page
+      .on("request", (req: HTTPRequest) => {
+        // console.log({
+        //   req: req.url(),
+        //   headers: req.headers(),
+        //   resourceType: req.resourceType(),
+        // });
+        if (["font", "image", "stylesheet"].includes(req.resourceType())) {
+          req.abort();
+        } else {
+          req.continue();
+        }
+      })
+      .on("response", async (res: HTTPResponse) => {
+        // const url = res.url();
+        const req = res.request();
+        const headers = res.headers();
 
-      if (
-        !["xhr"].includes(req.resourceType()) ||
-        !url.match("/hotShots/current")
-      ) {
-        return;
-      }
-      console.log(
-        Object.assign(
-          {
-            resourceType: req.resourceType(),
-            url,
-            // headers,
-          },
-          headers["content-type"] && {
-            contentType: headers["content-type"],
-          },
-          headers.location && {
-            location: headers.location,
+        console.log(url, req.resourceType());
+
+        if (url.match("goracy_strzal")) {
+          if (
+            ["xhr"].includes(req.resourceType()) &&
+            res.url().match("/hotShots/current")
+          ) {
+            console.log(["resolve.json"], res.url(), headers);
+            resolve({ json: await res.json() });
           }
-        )
-      );
+        } else {
+          if (["document"].includes(req.resourceType()) && !headers.location) {
+            console.log(["resolve.html"], res.url(), headers);
+            await delay();
+            // resolve({html: 'test'})
+            resolve({ html: await res.text() });
+          }
+        }
 
-      if (["fetch", "xhr"].includes(req.resourceType())) {
-        console.log(await res.json());
-      }
-    });
+        // if (
+        //   !["xhr"].includes(req.resourceType()) ||
+        //   !url.match("/hotShots/current")
+        // ) {
+        //   return;
+        // }
 
-  const response = await page.goto(url, {
-    // waitUntil: "networkidle2",
-    // timeout: 0,
+        return;
+
+        console.log(
+          Object.assign(
+            {
+              resourceType: req.resourceType(),
+              url,
+              // headers,
+            },
+            headers["content-type"] && {
+              contentType: headers["content-type"],
+            },
+            headers.location && {
+              location: headers.location,
+            }
+          )
+        );
+
+        if (["fetch", "xhr"].includes(req.resourceType())) {
+          const result = await res.json();
+          console.log(result);
+          // resolve(result)
+        }
+      });
   });
 
-  // await page.goto('https://example.com');
-  // await page.screenshot({path: 'example.png'});
-  if (response) {
-    console.log({
-      // text: await response.text(),
-      ok: response.ok(),
-      status: response.status(),
-      statusText: response.statusText(),
-    });
-  }
+  await page.goto(url, {
+    waitUntil: "networkidle2",
+    // timeout: 0,
+  });
+  // .then((response) => {
+  //   // await page.goto('https://example.com');
+  //   // await page.screenshot({path: 'example.png'});
+  //   if (response) {
+  //     console.log({
+  //       // text: await response.text(),
+  //       ok: response.ok(),
+  //       status: response.status(),
+  //       statusText: response.statusText(),
+  //     });
 
-  await delay();
+  //     // return response.text().then((html) => resolve({ html }));
+  //     // resolve('ok')
+  //   }
+  // });
 
-  await page.close();
-  await browser.close();
+  // await result;
+
+  return result.then(async (result) => {
+    // await delay();
+
+    await page.close();
+    await browser.close();
+
+    console.log({ result });
+    return result;
+  });
 }
 
 export default async function (url: string) {
--- packages/worker/src/index.ts
+++ packages/worker/src/index.ts
@@ -18,48 +18,66 @@ const { REDIS_URL, QUEUE_NAME } = z
   })
   .parse(process.env);
 
-const NAME = "task";
+const NAME = "chrome";
 
 export const client = () => new Queue(QUEUE_NAME, REDIS_URL);
 
-export const broker = async (number = "1", ...args: string[]) => {
-  console.log(["broker"], number, ...args);
+export const broker = async (
+  url = "https://zimekk.github.io/robot/",
+  ...args: string[]
+) => {
+  console.log(["broker"], url, ...args);
 
   const queue = client();
 
-  await queue
-    .add(
-      NAME,
-      {
-        number: Number(number),
-      },
-      {
-        delay: seconds(5),
-        // repeat: { cron: "1 10,22 * * *" },
-      }
-    )
-    .then(({ id, data }) => console.log(["add"], { id, data }));
+  // const NAME = "chrome";
+  // const url = 'https://www.al.to/goracy_strzal'
+  // const url = 'https://www.x-kom.pl/goracy_strzal'
+  // const url = 'http://www.makarewicz.eu'
+
+  await queue.add(
+    NAME,
+    {
+      url,
+    },
+    {
+      // delay: seconds(5),
+    }
+  );
+
+  // await queue
+  //   .add(
+  //     NAME,
+  //     {
+  //       number: Number(number),
+  //     },
+  //     {
+  //       delay: seconds(5),
+  //       // repeat: { cron: "1 10,22 * * *" },
+  //     }
+  //   )
+  //   .then(({ id, data }) => console.log(["add"], { id, data }));
 
   await queue.close().then(() => console.log(["close"]));
 };
 
-export const chrome = async (...args: string[]) => {
+export const chrome = async (...args: string[]) =>
   (await import("@dev/chrome")).chrome(...args);
-};
 
 export const router = () => {
   const BASE_PATH = "/board";
-  const NAME = "chrome";
+  // const NAME = "chrome";
 
   const serverAdapter = new ExpressAdapter();
   serverAdapter.setBasePath(BASE_PATH);
 
   const queue = client();
 
-  queue.process(NAME, async function ({ data }) {
-    console.log(["process"], data);
-    await chrome(data.url);
-  });
+  worker();
+  // queue.process(NAME, async function ({ data }) {
+  //   console.log(["process"], data);
+  //   return await chrome(data.url);
+  // });
 
   createBullBoard({
     queues: [new BullAdapter(queue)],
@@ -71,15 +89,17 @@ export const router = () => {
       const { url } = req.body;
       console.log(req.body);
 
-      await queue.add(
-        NAME,
-        {
-          url,
-        },
-        {
-          delay: seconds(5),
-        }
-      );
+      // await queue.add(
+      //   NAME,
+      //   {
+      //     url,
+      //   },
+      //   {
+      //     // delay: seconds(5),
+      //   }
+      // );
+
+      await broker(url);
 
       return res.json({ status: "ok" });
     })
@@ -115,8 +135,26 @@ export const worker = async () => {
 
   const queue = client();
 
-  queue.process(NAME, async function ({ data }) {
-    console.log(["process"], data);
-    return data.number + 1;
+  queue.process(NAME, async function (job) {
+    // queue.process(async function (job, done) {
+    const { data } = job;
+    console.log(["process"], NAME, data);
+    await job.log(`start processing ${NAME}`);
+    await job.progress(50);
+    const result = await chrome(data.url);
+
+    await job.progress(100);
+
+    // const result = 2;
+    await job.log(`end processing ${NAME}`);
+
+    console.log("process result", result);
+    // done(null,result)
+    return result;
   });
+
+  // queue.process(NAME, async function ({ data }) {
+  //   console.log(["process"], data);
+  //   return data.number + 1;
+  // });
 };
