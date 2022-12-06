ec8e000e2a390948caf1f8534011fa2ccf3a00a8
Marcin Makarewicz <marcin@makarewicz.eu>
2022-12-07 00:08:43 +0100

    feat(web): add delayed items

--- packages/web/src/containers/Hello.tsx
+++ packages/web/src/containers/Hello.tsx
@@ -312,6 +312,38 @@ export default function Section({ version = 1 }) {
             () =>
               post("process", {
                 data: {
+                  url: "https://www.autocentrum.pl/ac-ajax/stations-get-stations?zoom=6",
+                },
+                opts: {
+                  repeat: { cron: "0 11,19 * * *" },
+                },
+              }),
+            []
+          )}
+        >
+          stations-get-stations
+        </button>
+        <button
+          onClick={useCallback(
+            () =>
+              post("process", {
+                data: {
+                  url: "https://www.rbinternational.com.pl/rest/rates/?type=kursywalut&range=all",
+                },
+                opts: {
+                  repeat: { cron: "15 8 * * *" },
+                },
+              }),
+            []
+          )}
+        >
+          rest/rates
+        </button>
+        <button
+          onClick={useCallback(
+            () =>
+              post("process", {
+                data: {
                   url: "https://www.otodom.pl/pl/oferty/sprzedaz/dzialka/warszawa?limit=72&page=1",
                 },
                 opts: {
