909802fa3ad83ad702452e0496a2dd2f4e3cc94e
Marcin Makarewicz <marcin@makarewicz.eu>
2022-12-09 21:04:00 +0100

    refactor(worker): separate client and router

---
+++ packages/worker/src/client/index.ts
@@ -0,0 +1,238 @@
+import Queue from "bull";
+import { fetch } from "cross-fetch";
+import { format, sub } from "date-fns";
+import { config } from "dotenv";
+import { headingDistanceTo } from "geolocation-utils";
+import { seconds } from "milliseconds";
+import { resolve } from "path";
+import { z } from "zod";
+import { EntrySchema, Type } from "@dev/schema";
+
+config({ path: resolve(__dirname, "../../../.env") });
+
+const { REDIS_URL, QUEUE_NAME } = z
+  .object({
+    REDIS_URL: z.string().default("redis://127.0.0.1:6379"),
+    QUEUE_NAME: z.string().default("robot"),
+  })
+  .parse(process.env);
+
+type Data = { url: string; body?: object };
+
+export const chrome = async (...args: string[]) =>
+  (await import("@dev/chrome")).chrome(...args);
+
+export const client = () => {
+  const NAME = "chrome";
+  const queue = new Queue<Data>(QUEUE_NAME, REDIS_URL, {
+    limiter: {
+      max: 1, // Max number of jobs processed
+      duration: 15000, // per duration in milliseconds
+    },
+  });
+
+  const q = {
+    async produce(
+      data: Data,
+      opts = {
+        delay: seconds(1),
+        // repeat: { cron: "1 10,22 * * *" },
+      }
+    ) {
+      await queue.add(NAME, data, opts);
+      return q;
+    },
+    process() {
+      queue
+        .on(
+          "completed",
+          async ({ id, name, data, opts, finishedOn }, returnvalue) => {
+            console.log(["completed"], { id, name, data, opts, finishedOn });
+
+            const jobs = await queue.getJobs(["completed", "active"]);
+
+            const urls = jobs.map(({ data }) => data.url);
+
+            console.log(urls);
+
+            await EntrySchema.parseAsync({ id, data, returnvalue }).then(
+              async ({ data, type, returnvalue }) => {
+                if (type === Type.AUTOS) {
+                  const { $count } = returnvalue.json;
+
+                  return z
+                    .object({
+                      url: z.string(),
+                      body: z
+                        .object({
+                          $match: z.object({}).passthrough(),
+                          $skip: z.number(),
+                          $limit: z.number(),
+                        })
+                        .transform(({ $skip, $limit, ...body }) => ({
+                          ...body,
+                          $skip: $skip + $limit,
+                          $limit,
+                        })),
+                    })
+                    .parseAsync(data)
+                    .then((data) =>
+                      data.body.$skip < $count.$total ? q.produce(data) : null
+                    );
+                } else if (type === Type.OTODOM) {
+                  const { items } =
+                    returnvalue.json.props.pageProps.data?.searchAds || {};
+                  return (
+                    items &&
+                    Promise.resolve(
+                      items
+                        .map(({ slug }) => ({
+                          url: `${new URL(data.url).origin}/pl/oferta/${slug}`,
+                        }))
+                        .filter(({ url }) => !urls.includes(url))
+                      // .slice(0, 15)
+                    )
+                      .then((list) => (console.log({ list }), list))
+                      .then((list) =>
+                        Promise.all(list.map((data) => q.produce(data)))
+                      )
+                  );
+                } else if (type === Type.PROMO) {
+                  return Promise.resolve(
+                    returnvalue.json.list
+                      .map((data) => ({ ...data, url: data.href }))
+                      .filter(({ url }) => new RegExp("//promocje.").test(url))
+                      .filter(({ url }) => !urls.includes(url))
+                    // .slice(0, 15)
+                  )
+                    .then((list) => (console.log({ list }), list))
+                    .then((list) =>
+                      Promise.all(list.map((data) => q.produce(data)))
+                    );
+                } else if (type === Type.RATES) {
+                  return Promise.resolve(returnvalue.json)
+                    .then(({ date }: { date: string }) =>
+                      format(
+                        sub(new Date(date), {
+                          days: 1,
+                        }),
+                        "yyyy-MM-dd"
+                      )
+                    )
+                    .then((date) => {
+                      if (
+                        date <
+                        format(
+                          sub(new Date(), {
+                            days: 7,
+                          }),
+                          "yyyy-MM-dd"
+                        )
+                      ) {
+                        return [];
+                      }
+                      const url = new URL(data.url);
+                      url.searchParams.set("date", date);
+                      return [{ url: url.toString() }]
+                        .filter(({ url }) => !urls.includes(url))
+                        .filter(({ url }) => !urls.includes(url));
+                    })
+                    .then((list) => (console.log({ list }), list))
+                    .then((list) =>
+                      Promise.all(list.map((data) => q.produce(data)))
+                    );
+                } else if (type === Type.STATIONS) {
+                  const { url } = data;
+                  return Promise.resolve(
+                    returnvalue.json
+                      .map((data: any) => ({
+                        ...data,
+                        url: `${
+                          new URL(url).origin
+                        }/ac-ajax/stations-get-station?station_id=${
+                          data.station_id
+                        }`,
+                      }))
+                      .filter(({ url }: any) => !urls.includes(url))
+
+                      .filter(
+                        ({ x: lat, y: lng }: any) =>
+                          [
+                            `${52.17}:${21.06}:${10000}`,
+                            `${52.46}:${21.29}:${10000}`,
+                            `${53.12}:${23.09}:${10000}`,
+                          ]
+                            .map((line) =>
+                              z
+                                .tuple([
+                                  z.string().transform(Number),
+                                  z.string().transform(Number),
+                                  z.string().transform(Number),
+                                ])
+                                .transform(([lat, lng, $radius]) => ({
+                                  $center: { lat, lng },
+                                  $radius,
+                                }))
+                                .parse(line.split(":"))
+                            )
+                            .findIndex(
+                              ({ $center, $radius }) =>
+                                headingDistanceTo($center, { lat, lng })
+                                  .distance < $radius
+                            ) >= 0
+                      )
+                      .slice(0, 250)
+                  )
+                    .then((list) => (console.log({ list }), list))
+                    .then((list) =>
+                      Promise.all(list.map((data: any) => q.produce(data)))
+                    );
+                }
+              }
+            );
+          }
+        )
+        .process(NAME, async function (job) {
+          const { data } = job;
+
+          console.log(["process"], NAME, data);
+          await job.log(`process ${NAME}`);
+          await job.progress(50);
+
+          const returnvalue = data.body
+            ? await fetch(data.url, {
+                method: "post",
+                body: JSON.stringify(data.body),
+              }).then(
+                async (res) =>
+                  ({ url: data.url, json: await res.json() } as {
+                    url: string;
+                    html?: string | undefined;
+                    json?: object | undefined;
+                  })
+              )
+            : await chrome(data.url);
+
+          await job.progress(100);
+
+          if (returnvalue.html && returnvalue.url !== data.url) {
+            console.log(["failure"], NAME);
+            throw new Error(`Invalid response url: ${returnvalue.url}`);
+          }
+
+          console.log(["success"], NAME);
+          await job.log(`success ${NAME}`);
+
+          return returnvalue;
+        });
+
+      return q;
+    },
+    close() {
+      return queue.close();
+    },
+    queue,
+  };
+
+  return q;
+};
---
+++ packages/worker/src/router/index.ts
@@ -0,0 +1,150 @@
+import { json } from "body-parser";
+import { Router } from "express";
+import { createBullBoard } from "@bull-board/api";
+import { BullAdapter } from "@bull-board/api/bullAdapter";
+import { ExpressAdapter } from "@bull-board/express";
+import { z } from "zod";
+import { EntrySchema } from "@dev/schema";
+
+import { client } from "../client";
+
+export const router = () => {
+  const BASE_PATH = "/board";
+
+  const serverAdapter = new ExpressAdapter();
+  serverAdapter.setBasePath(BASE_PATH);
+
+  const worker = client();
+
+  worker.process();
+
+  createBullBoard({
+    queues: [new BullAdapter(worker.queue)],
+    serverAdapter,
+  });
+
+  return Router()
+    .post("/process", json(), async (req, res) => {
+      const { data, opts } = await z
+        .object({
+          data: z.object({
+            url: z.string(),
+            body: z.object({}).passthrough().optional(),
+          }),
+          opts: z
+            .object({
+              delay: z.number().default(0),
+              repeat: z
+                .object({
+                  cron: z.string().optional(),
+                })
+                .optional(),
+            })
+            .optional(),
+        })
+        .parseAsync(req.body);
+
+      await worker.produce(data, opts);
+
+      return res.json({ status: "ok" });
+    })
+    .post("/entries/delete", json(), async (req, res) =>
+      z
+        .object({
+          selected: z.string().array(),
+        })
+        .parseAsync(req.body)
+        .then(async ({ selected }) =>
+          selected
+            .reduce(
+              (promise, id) => promise.then(() => worker.queue.removeJobs(id)),
+              Promise.resolve()
+            )
+            .then(() => res.json({ status: "ok" }))
+        )
+    )
+    .post("/entries", json(), async (req, res) =>
+      z
+        .object({
+          start: z.number().default(0),
+          limit: z.number().default(250),
+          type: z.string().default(""),
+          data: z.boolean().default(false),
+          returnvalue: z.boolean().default(true),
+        })
+        .parseAsync(req.body)
+        .then(async ({ start, limit, type, data }) => {
+          const list = await worker.queue.getCompleted(
+            start,
+            start + limit - 1
+          );
+          return EntrySchema.array()
+            .parseAsync(list)
+            .then((list) =>
+              list.filter((item) => type === "" || item.type === type)
+            )
+            .then((list) =>
+              data
+                ? z
+                    .object({
+                      id: z.string(),
+                      data: z.object({
+                        url: z.string(),
+                      }),
+                    })
+                    .array()
+                    .parseAsync(list)
+                : list
+            );
+        })
+        .then((entries) => res.json(entries))
+    )
+    .get("/entry/:id", async (req, res) =>
+      Promise.resolve(req.params).then(({ id }) =>
+        worker.queue
+          .getJob(id)
+          .then((item) => EntrySchema.parseAsync(item))
+          .then((json) => res.send(json))
+      )
+    )
+    .get("/delete/:id", async (req, res) =>
+      Promise.resolve(req.params).then(({ id }) =>
+        worker.queue.removeJobs(id).then(() => res.json({ status: "ok" }))
+      )
+    )
+    .get("/:type/:id/", async (req, res) =>
+      Promise.resolve(req.params).then(({ id, type = "" }) =>
+        worker.queue
+          .getJob(id)
+          .then((job) =>
+            z
+              .union([
+                z.null(),
+                z.object({
+                  id: z.string(),
+                  name: z.string(),
+                  data: z.any(),
+                  returnvalue: z.any(),
+                }),
+              ])
+              .parseAsync(job)
+          )
+          .then((item) => (item ? item.returnvalue[type] || item : null))
+          .then((json) => (type === "html" ? res.send(json) : res.json(json)))
+      )
+    )
+    .post("/cleanup", async (_req, res) => {
+      const queue = worker.queue;
+      await Promise.all(
+        (
+          await queue.getRepeatableJobs()
+        ).map(
+          async ({ key }) =>
+            Boolean(console.log(["cleanup"], { key })) ||
+            (await queue.removeRepeatableByKey(key))
+        )
+      );
+      return res.json({ status: "ok" });
+    })
+    .use(BASE_PATH, serverAdapter.getRouter());
+};
