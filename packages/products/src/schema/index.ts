55428c62f8b222db274492e9eeef27f0e707e470
Marcin Makarewicz <marcin@makarewicz.eu>
2023-02-07 23:47:10 +0100

    fix(products): product schema

--- packages/products/src/schema/index.ts
+++ packages/products/src/schema/index.ts
@@ -6,44 +6,52 @@ export const PhotoSchema = z.object({
   urlTemplate: z.string(),
 });
 
-export const ProductSchema = z.object({
-  featureSummary: z.string().array(),
-  featureSummaryStructured: z
-    .object({
-      shortName: z.string(),
-      description: z.null(),
-      valueSeparator: z.string(),
-      valueGroups: z
-        .object({ shortName: z.string(), description: z.null() })
-        .array(),
-    })
-    .array(),
-  availabilityStatus: z.string(),
-  producerCode: z.string(),
-  installmentMinimum: z.number(),
-  alternativeGroupId: z.string().optional(),
-  alternativeProducts: z.unknown().array().optional(),
-  priceInfo: z.object({
+export const ProductSchema = z
+  .object({
+    featureSummary: z.string().array().optional(),
+    featureSummaryStructured: z
+      .object({
+        shortName: z.string(),
+        description: z.string().nullable(),
+        valueSeparator: z.string(),
+        valueGroups: z
+          .object({ shortName: z.string(), description: z.null() })
+          .array(),
+      })
+      .array()
+      .optional(),
+    availabilityStatus: z.enum(["Available", "Unavailable"]).optional(),
+    producerCode: z.string().optional(),
+    installmentMinimum: z.number().optional(),
+    alternativeGroupId: z.string().optional(),
+    alternativeProducts: z.unknown().array().optional(),
+    mark: z.string().optional(),
+    oldPrice: z.number().optional(),
+    priceInfo: z.object({
+      price: z.number(),
+      oldPrice: z.number().nullable(),
+      isPriceVisible: z.boolean(),
+    }),
+    producer: z.object({ id: z.string(), name: z.string() }),
+    isEsd: z.boolean(),
+    esdType: z.string(),
+    productLink: z.string(),
+    photo: PhotoSchema,
+    rating: z.number().optional(),
+    ratingCount: z.number().optional(),
+    commentsCount: z.number().optional(),
+    freeShipping: z.boolean().optional(),
+    id: z.string(),
+    name: z.string(),
     price: z.number(),
-    oldPrice: z.null(),
-    isPriceVisible: z.boolean(),
-  }),
-  producer: z.object({ id: z.string(), name: z.string() }),
-  isEsd: z.boolean(),
-  esdType: z.string(),
-  productLink: z.string(),
-  photo: PhotoSchema,
-  rating: z.number(),
-  ratingCount: z.number(),
-  commentsCount: z.number(),
-  freeShipping: z.boolean(),
-  id: z.string(),
-  name: z.string(),
-  price: z.number(),
-  category: z.object({ id: z.string(), parentGroupId: z.string() }),
-  questionsAndAnswers: z.boolean(),
-  isFetching: z.boolean(),
-});
+    category: z.object({
+      id: z.string(),
+      parentGroupId: z.string().optional(),
+    }),
+    questionsAndAnswers: z.boolean(),
+    isFetching: z.boolean(),
+  })
+  .strict();
 
 export type Product = z.infer<typeof ProductSchema>;
 
