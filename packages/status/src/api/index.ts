5db5f6885e8de277bf1cca37defa96fd929ec71f
Marcin Makarewicz <marcin@makarewicz.eu>
2025-07-16 10:03:16 +0200

    feat(status): fetch url status

--- packages/status/src/api/index.ts
+++ packages/status/src/api/index.ts
@@ -7,10 +7,16 @@ import sslChecker from "ssl-checker";
 import type { SchemaType } from "../schema";
 import { getTotal } from "../utils";
 
-async function notify(data: { total?: number; ssl?: object[] }) {
-  const subject = data.ssl
-    ? `Robot Notification - SSL`
-    : `Robot Notification - Usage ${data.total}%`;
+async function notify(data: {
+  total?: number;
+  ssl?: object[];
+  url?: Record<string, boolean>;
+}) {
+  const subject = data.url
+    ? `Robot Notification - URL`
+    : data.ssl
+      ? `Robot Notification - SSL`
+      : `Robot Notification - Usage ${data.total}%`;
   const text = JSON.stringify(data, null, 2);
 
   const transporter = createTransport(process.env.SMTP_URL, {
@@ -49,6 +55,22 @@ const sslCheck = () =>
       .map((hostname) => sslChecker(hostname, { method: "GET", port: 443 })),
   );
 
+const urlCheck = () =>
+  Promise.all(
+    (process.env.URL_CHECK || "")
+      .split("|")
+      .concat([
+        // "http://example.com/",
+      ])
+      .filter(Boolean)
+      .map((url) =>
+        fetch(url)
+          .then((res) => res.ok)
+          .catch(() => false)
+          .then((ok) => ({ url, ok })),
+      ),
+  );
+
 export const router = () =>
   Router().get("/status", (_req, res, next) =>
     Promise.all([
@@ -57,24 +79,31 @@ export const router = () =>
         ? query(
             "SELECT pg_database.datname AS name, pg_size_pretty(pg_database_size(pg_database.datname)) AS size FROM pg_database",
             [],
-          )
-        : {
-            rows: [],
-          },
+          ).catch(() => null)
+        : null,
       process.env.DATABASE_URL
         ? query(
             "SELECT table_name AS name, pg_size_pretty(pg_total_relation_size(quote_ident(table_name))) AS size FROM information_schema.tables where table_schema=$1;",
             ["public"],
-          )
-        : {
-            rows: [],
-          },
+          ).catch(() => null)
+        : null,
       sslCheck(),
+      urlCheck().then((url) =>
+        url.reduce(
+          (result, { url, ok }) =>
+            Object.assign(result, {
+              [url.substring(0, 15)]: ok,
+            }),
+          {},
+        ),
+      ),
     ])
-      .then(([usage, data, tables, ssl]) =>
+      .then(([usage, data, tables, ssl, url]) =>
         res.json({
           result: {
-            databases: ([] as any[]).concat(data.rows).concat(tables.rows),
+            databases: ([] as any[])
+              .concat(data ? data.rows : [])
+              .concat(tables ? tables.rows : []),
             cpus: os.cpus(),
             freemem: os.freemem(),
             totalmem: os.totalmem(),
@@ -86,6 +115,7 @@ export const router = () =>
             usage,
             total: getTotal(usage),
             ssl,
+            url,
           },
         }),
       )
@@ -98,12 +128,24 @@ export const status = async (data: SchemaType) =>
     sslCheck().then((ssl) =>
       ssl.filter(({ daysRemaining }) => daysRemaining <= 20),
     ),
-  ]).then(async ([total, ssl]) => {
+    urlCheck(),
+  ]).then(async ([total, ssl, url]) => {
     if (total > 95) {
       await notify({ total });
     }
     if (ssl.length > 0) {
       await notify({ ssl });
+    }
+    if (url.filter(({ ok }) => !ok).length > 0) {
+      await notify({
+        url: url.reduce(
+          (result, { url, ok }) =>
+            Object.assign(result, {
+              [url]: ok,
+            }),
+          {},
+        ),
+      });
     }
     return {
       json: {
